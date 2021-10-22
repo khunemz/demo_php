7c648404a817a18b62a1eb3eadcd3db71f0a9d42
khunemz <khunemz@gmail.com>
2021-10-22 15:34:14 +0700

    add drawer

---
+++ llv-huey/resources/js/components/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from './../extendTheme'
+import Navbar from "./Navbar";
+
+function App() {
+    return (
+        <ChakraProvider theme={theme}>
+            <Navbar />
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">App Component</div>
+
+                            <div className="card-body">
+                                Laravel Application
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </ChakraProvider>
+    );
+}
+
+export default App;
+
+if (document.getElementById("myapp")) {
+    ReactDOM.render(<App />, document.getElementById("myapp"));
+}
